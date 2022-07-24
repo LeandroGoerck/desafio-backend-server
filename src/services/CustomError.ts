f945a8f2caba554fdf24ec6ed9b50f73c553cd66
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-07-24 13:56:52 -0300

    refactor: change custom erro from private to publi

--- src/services/CustomError.ts
+++ src/services/CustomError.ts
@@ -1,6 +1,6 @@
 export default class CustomError extends Error {
   public name = 'CustomError';
-  constructor(public message: string, private status: number) {
+  constructor(public message: string, public status: number) {
     super(message);
     this.status = status;
   }
