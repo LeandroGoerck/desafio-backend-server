c794eb5acde3704c1ead3c6d0f7674ee84486ff3
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-07-24 13:58:56 -0300

    feat: add zod to validate requests

--- src/services/registerService.ts
+++ src/services/registerService.ts
@@ -2,10 +2,18 @@ import bcryptjs from 'bcryptjs';
 import UserModel from '../database/models/UserModel';
 import IUser from '../interfaces/IUser';
 import ERR from './errors';
+import zod from 'zod';
+
+const registerShema = zod.object({
+  email: zod.string().email(),
+  name: zod.string().min(2).max(15),
+  password: zod.string().min(6),
+});
 
 export default class RegisterService {
   public register = async (register: IUser) => {
-    const { name, email, password } = register;
+    const { name, email, password } = registerShema.parse(register);
+
     const salt = bcryptjs.genSaltSync(10);
     const hash = bcryptjs.hashSync(password, salt);
 
