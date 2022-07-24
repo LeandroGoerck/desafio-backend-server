4e8703a6aa55414a70eff1fd1f7f32dc3a1a9d88
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-07-24 12:22:15 -0300

    feat: create error Middleware

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+
+const validateToken = (
+  (err: { status: number, message: string }, req: Request, res: Response, next: NextFunction) => {
+    const { status, message } = err;
+    if (status !== undefined) return res.status(status).json({ message });
+    console.log(err);
+    res.status(500).json({ message: 'Internal server Error' });
+    next();
+  });
+
+export default validateToken;
